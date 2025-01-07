ba3795ddc92d26d4fda40224e74b57d74954735d
idhyt <idhyt3r@gmail.com>
2025-01-07 15:18:38 +0800

    ✨ Init and testcase

--- builder/place/src/meta.rs
+++ builder/place/src/meta.rs
@@ -1,9 +1,12 @@
 use anyhow::{anyhow, Result};
+use lazy_static::lazy_static;
 use std::collections::HashSet;
-use std::path::{Path, PathBuf};
-use std::process::Stdio;
+use std::path::PathBuf;
 use tokio::io::{AsyncBufReadExt, BufReader};
-use tokio::process::Command;
+
+lazy_static! {
+    pub static ref META: MetadataReader = MetadataReader::new(None);
+}
 
 #[derive(Debug, Clone)]
 pub struct MetadataReader {
@@ -12,57 +15,36 @@ pub struct MetadataReader {
 }
 
 impl MetadataReader {
-    pub async fn new<T>(tool_dir: T) -> Result<Self>
-    where
-        T: AsRef<Path>,
-    {
-        let jlibs_dir = tool_dir.as_ref();
-        let extractor_jar = jlibs_dir.join("metadata-extractor-2.18.0.jar");
-        if !extractor_jar.is_file() {
-            return Err(anyhow!(
-                "metadata-extractor file not found in {:?}",
-                extractor_jar
-            ));
-        }
+    pub fn new(tool_dir: Option<&PathBuf>) -> Self {
+        let tool_dir = if let Some(t) = tool_dir {
+            t
+        } else {
+            let mut w = std::env::current_exe().unwrap();
+            w.pop();
+            &w.join("tools")
+        };
+        assert!(
+            tool_dir.is_dir(),
+            "tool dir not found in {}",
+            tool_dir.display()
+        );
 
-        let xmpcore_jar = jlibs_dir.join("xmpcore-6.1.11.jar");
-        if !xmpcore_jar.is_file() {
-            return Err(anyhow!("xmpcore file not found in {:?}", xmpcore_jar));
-        }
+        let extractor_jar = tool_dir.join("metadata-extractor.jar");
+        assert!(extractor_jar.is_file(), "extractor jar not found");
+
+        let xmpcore_jar = tool_dir.join("xmpcore.jar");
+        assert!(xmpcore_jar.is_file(), "xmpcore jar not found");
 
         // check java runtime
-        match Command::new("java")
-            .arg("-version")
-            .stderr(Stdio::piped())
-            .output()
-            .await
-        {
-            Ok(output) => match output.status.success() {
-                true => log::debug!(
-                    "java runtime found: {}",
-                    String::from_utf8_lossy(&output.stderr)
-                ),
-                false => {
-                    return Err(anyhow!(
-                        "java runtime not found, please install java runtime first."
-                    ));
-                }
-            },
-            Err(err) => {
-                return Err(anyhow!(
-                    "java runtime not found, please install java runtime first, {:?}",
-                    err
-                ));
-            }
-        }
-
-        Ok(MetadataReader {
+        let output = std::process::Command::new("java").arg("-version").output();
+        assert!(output.is_ok(), "java runtime not found");
+        MetadataReader {
             extractor_jar,
             xmpcore_jar,
-        })
+        }
     }
 
-    pub async fn read(&self, file_path: impl AsRef<Path>) -> Result<HashSet<String>> {
+    pub async fn read(&self, file_path: &PathBuf) -> Result<HashSet<String>> {
         let mut readers: HashSet<String> = HashSet::new();
         let class_path = format!(
             "{xc_jar}{c}{me_jar}",
@@ -74,13 +56,15 @@ impl MetadataReader {
             "-cp",
             &class_path,
             "com.drew.imaging.ImageMetadataReader",
-            file_path.as_ref().as_os_str().to_str().unwrap(),
+            file_path.to_str().unwrap(),
         ];
-        log::debug!("run command args: {}", args.join(" "));
-        let mut child = Command::new("java")
+
+        log::debug!("run command args: {:?}", args);
+
+        let mut child = tokio::process::Command::new("java")
             // .current_dir(file_path.as_ref())
             .args(args)
-            .stdout(Stdio::piped())
+            .stdout(std::process::Stdio::piped())
             .spawn()?;
 
         let stdout = match child.stdout.take() {
@@ -97,13 +81,6 @@ impl MetadataReader {
             }
         });
 
-        // while let Some(line) = reader.next_line().await? {
-        //     log::debug!("{}", line);
-        //     if line.len() < 0xff {
-        //         readers.insert(line);
-        //     }
-        // }
-
         // stream did not contain valid UTF-8
         while let Some(line) = match reader.next_line().await {
             Ok(line) => line,
@@ -113,10 +90,34 @@ impl MetadataReader {
             }
         } {
             log::debug!("{}", line);
+            // only insert if len < 0xff
             if line.len() < 0xff {
                 readers.insert(line);
             }
         }
         Ok(readers)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let meta = MetadataReader::new(None);
+        println!("{:#?}", meta);
+        assert!(meta.extractor_jar.is_file());
+    }
+
+    #[tokio::test]
+    async fn test_read() {
+        let meta = MetadataReader::new(None);
+        let test = PathBuf::from("/tmp/mmfplace-tests/simple.jpg");
+        let readers = meta.read(&test).await.unwrap();
+        println!("{:#?}", readers);
+        assert!(readers.len() > 1);
+        assert!(readers.contains("[Exif SubIFD] Date/Time Original = 2002:11:16 15:27:01"));
     }
 }
