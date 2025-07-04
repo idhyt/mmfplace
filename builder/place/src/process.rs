61354f933370def7db6a492804bd80607060c911
idhyt <idhyt3r@gmail.com>
2025-07-04 20:30:44 +0800

    feat: place implement

--- builder/place/src/process.rs
+++ builder/place/src/process.rs
@@ -2,14 +2,14 @@ use anyhow::Result;
 use chrono::Datelike;
 use chrono::{DateTime, Utc};
 use once_cell::sync::OnceCell;
-use std::os::windows::fs::FileTimesExt;
 use std::path::Path;
 use std::path::PathBuf;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
+use std::time::SystemTime;
 use tokio::sync::mpsc;
 use tokio::sync::Semaphore;
-use tracing::{debug, debug_span, info, warn};
+use tracing::{debug, debug_span, error, info, warn};
 use tracing_futures::Instrument;
 use walkdir::WalkDir;
 
@@ -23,7 +23,7 @@ use utils::crypto::get_file_md5;
 struct Target {
     // target file path
     path: PathBuf,
-    // parsed datetime
+    // parsed datetime from metadata
     datetimes: Vec<DateTime<Utc>>,
     // hash with md5
     hash: String,
@@ -32,9 +32,12 @@ struct Target {
     // the file name without extension
     name: String,
     // the file parsed type
-    ptype: Option<String>,
+    pub type_: Option<String>,
     // the earliest datetime
     earliest: DateTime<Utc>,
+    // datetime from file attributes
+    // [accessed, modified, created]
+    attrtimes: Vec<Option<SystemTime>>,
 }
 
 // 临时共享数据，我不知道该取什么名字hhh...
@@ -169,7 +172,6 @@ pub async fn do_process(input: &Path, output: Option<&Path>, test: bool) -> Resu
 //                                      -> 不存在 -> 解析所有时间(元数据+文件属性) -> 取最早 -> 插入数据库 -> 拷贝文件
 async fn do_parse(path: PathBuf) -> Result<Option<Target>> {
     info!("🚀 begin parse file: {:?}", path);
-    // 计算文件hash
     let target = Target::new(path);
     let parts = {
         let conn = get_connection().lock().unwrap();
@@ -180,14 +182,46 @@ async fn do_parse(path: PathBuf) -> Result<Option<Target>> {
         debug!(hash = target.hash, "file is already parsed");
         return Ok(Some(target));
     }
-    // 获取文件元数据并解析出所有时间格式
-    let _readers = metadata_extractor(&target.path).await?;
-    // TODO: 解析时间
 
-    Ok(None)
+    let mut target = target;
+    // 获取文件元数据并解析出所有时间格式
+    let texts = metadata_extractor(&target.path).await?;
+    'outer: for text in texts.iter() {
+        // 过滤字符串
+        for black in &CONFIG.blacklist {
+            if text.contains(black) {
+                debug!(black = black, text = text, "skip black string");
+                continue 'outer;
+            }
+        }
+        // // TODO: 获取文件type
+        // if target.type_.is_none() {
+        //     if let Some(t) = capture_type(&value) {
+        //         info!(
+        //             path=?target.path,
+        //             type_ = t,
+        //             "🏷️ success capture file type from metadata",
+        //         );
+        //         // println!("capture file extension from metadata: {}", t);
+        //         target.type_ = t;
+        //     }
+        // }
+        if let Ok(dt) = dateparser::parse(text) {
+            if dt.year() < 1975 {
+                warn!(path=?target.path, datetime=%dt, "💡 skip the datetime < 1975");
+            } else {
+                info!(text = text, datetime = %dt, "success date parse");
+                target.add_datetime(dt);
+            }
+        }
+    }
+    target.set_earliest();
+
+    Ok(Some(target))
 }
 
 async fn do_place(target: Target, processed_count: &Arc<AtomicUsize>) -> Result<()> {
+    let count = processed_count.fetch_add(1, Ordering::SeqCst) + 1;
     debug!(
         "🚚 begin place file: {:?}, count: {:?}",
         target.path, processed_count
@@ -249,8 +283,14 @@ async fn do_place(target: Target, processed_count: &Arc<AtomicUsize>) -> Result<
             true
         }
     };
+
+    if temp_get().test {
+        info!(from=?target.path, to=?copy_path, count=count, "test success finish");
+        return Ok(());
+    }
+
     if need_copy {
-        copy_file_with_times(&target.path, &copy_path)?;
+        copy_file_with_times(&target.path, &copy_path, &target.attrtimes)?;
     }
     if !exist {
         // 插入数据库
@@ -265,46 +305,56 @@ async fn do_place(target: Target, processed_count: &Arc<AtomicUsize>) -> Result<
         debug!(path=?copy_path, "success insert hash");
     }
 
-    let count = processed_count.fetch_add(1, Ordering::SeqCst) + 1;
     info!(from=?target.path, to=?copy_path, count=count, "success finish");
     Ok(())
 }
 
-fn copy_file_with_times(src: &Path, dst: &Path) -> Result<()> {
+fn copy_file_with_times(src: &Path, dst: &Path, times: &Vec<Option<SystemTime>>) -> Result<()> {
     let dir = dst.parent().unwrap();
     if !dir.exists() {
         std::fs::create_dir_all(dir)?;
     }
     std::fs::copy(src, dst)?;
 
-    let src_meta = std::fs::metadata(src)?;
-    let mut times = std::fs::FileTimes::new();
+    if times.len() != 3 {
+        return Err(anyhow::anyhow!("the attributes time invalid! {:?}", times));
+    }
+    let mut new_times = std::fs::FileTimes::new();
+    if let Some(atime) = times[0] {
+        new_times = new_times.set_accessed(atime);
+    }
+    if let Some(mtime) = times[1] {
+        new_times = new_times.set_accessed(mtime);
+    }
+    if let Some(ctime) = times[2] {
+        new_times = new_times.set_accessed(ctime);
+    }
 
-    if let Ok(atime) = src_meta.accessed() {
-        times = times.set_accessed(atime);
-    } else {
-        warn!(file=?src, "💡 accessed time not found");
-    }
-    if let Ok(mtime) = src_meta.modified() {
-        times = times.set_modified(mtime);
-    } else {
-        warn!(file=?src, "💡 modified time not found");
-    }
-    if let Ok(ctime) = src_meta.created() {
-        times = times.set_created(ctime);
-    } else {
-        warn!(file=?src, "💡 created time not found");
-    }
+    // if let Ok(atime) = src_meta.accessed() {
+    //     times = times.set_accessed(atime);
+    // } else {
+    //     warn!(file=?src, "💡 accessed time not found");
+    // }
+    // if let Ok(mtime) = src_meta.modified() {
+    //     times = times.set_modified(mtime);
+    // } else {
+    //     warn!(file=?src, "💡 modified time not found");
+    // }
+    // if let Ok(ctime) = src_meta.created() {
+    //     times = times.set_created(ctime);
+    // } else {
+    //     warn!(file=?src, "💡 created time not found");
+    // }
     std::fs::File::options()
         .write(true)
         .open(dst)?
-        .set_times(times)?;
+        .set_times(new_times)?;
     Ok(())
 }
 
 impl Target {
     pub fn new(path: PathBuf) -> Self {
-        Target {
+        let mut target = Target {
             hash: get_file_md5(&path).unwrap(),
             extension: path
                 .extension()
@@ -314,7 +364,9 @@ impl Target {
                 .map_or("NoName".to_string(), |n| n.to_string_lossy().to_lowercase()),
             path,
             ..Default::default()
-        }
+        };
+        target.set_attrtimes();
+        target
     }
 
     // 重名文件添加序号
@@ -323,14 +375,14 @@ impl Target {
             format!(
                 "{}.{}",
                 self.name,
-                self.ptype.as_ref().map_or(&self.extension, |s| &s)
+                self.type_.as_ref().map_or(&self.extension, |s| &s)
             )
         } else {
             format!(
                 "{}_{:02}.{}",
                 self.name,
                 i,
-                self.ptype.as_ref().map_or(&self.extension, |s| &s)
+                self.type_.as_ref().map_or(&self.extension, |s| &s)
             )
         }
     }
@@ -342,5 +394,55 @@ impl Target {
             self.earliest.day().to_string(),
             self.get_name(i),
         ]
+    }
+
+    pub fn add_datetime(&mut self, dt: DateTime<Utc>) {
+        self.datetimes.push(dt);
+    }
+
+    pub fn set_attrtimes(&mut self) {
+        let meta = std::fs::metadata(&self.path).unwrap();
+        if let Ok(atime) = meta.accessed() {
+            self.attrtimes.push(Some(atime));
+        } else {
+            warn!(file=?self.path, "💡 accessed time not found");
+            self.attrtimes.push(None);
+        }
+        if let Ok(mtime) = meta.modified() {
+            self.attrtimes.push(Some(mtime));
+        } else {
+            warn!(file=?self.path, "💡 modified time not found");
+            self.attrtimes.push(None);
+        }
+        // #[cfg(windows)] only support in Windows
+        if let Ok(ctime) = meta.created() {
+            self.attrtimes.push(Some(ctime));
+        } else {
+            debug!(file=?self.path, "💡 created time not found(Non-Windows?)");
+            self.attrtimes.push(None);
+        }
+    }
+
+    pub fn set_earliest(&mut self) {
+        if self.datetimes.is_empty() {
+            // should panic?
+            warn!(file=?self.path, "💡 datetime not found by dateparser")
+        }
+        let mut all = self
+            .attrtimes
+            .iter()
+            .filter_map(|ost| ost.as_ref().map(|st| DateTime::<Utc>::from(*st)))
+            .collect::<Vec<DateTime<Utc>>>();
+        all.extend(self.datetimes.clone());
+
+        if all.is_empty() {
+            // self.earliest = Utc::now();
+            // should panic
+            error!(file=?self.path, "💥 datetime not found by dateparser and attributes!");
+            panic!()
+        }
+        // min
+        self.earliest = all.into_iter().min().unwrap();
+        debug!(file=?self.path, earliest = ?self.earliest, "success set earliest datetime");
     }
 }
